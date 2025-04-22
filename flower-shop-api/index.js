a38d655776c3460182f677bc324bd9cdcf3d881f
Jonna Luthman <jonna.luthman@medieinstitutet.se>
2025-04-22 14:36:32 +0100

    API endpoint order items done.

--- flower-shop-api/index.js
+++ flower-shop-api/index.js
@@ -24,9 +24,8 @@ export const connectDB = async () => {
     res.send({ test: "test" });
   });
 
-  const customersCollection = await database.collection("customers");
-
   //   Customers API endpoints
+  const customersCollection = await database.collection("customers");
   app.get("/customers", async (req, res) => {
     try {
       const customers = await customersCollection.find().toArray();
@@ -141,10 +140,20 @@ export const connectDB = async () => {
 
   // Orders API endpoints
   const ordersCollection = await database.collection("orders");
-
   app.get("/orders", async (req, res) => {
     try {
-      const orders = await ordersCollection.find().toArray();
+      const orders = await ordersCollection
+        .aggregate([
+          {
+            $lookup: {
+              from: "order_items",
+              localField: "_id",
+              foreignField: "order_id",
+              as: "order_items",
+            },
+          },
+        ])
+        .toArray();
 
       if (!orders) {
         return res.status(404).send({ error: "Could not fetch orders" });
@@ -157,18 +166,32 @@ export const connectDB = async () => {
   });
 
   app.get("/orders/:id", async (req, res) => {
-    const ordersId = req.params.id;
+    const orderId = req.params.id;
 
     try {
-      const order = await ordersCollection.findOne({
-        _id: new ObjectId(ordersId),
-      });
+      const order = await ordersCollection
+        .aggregate([
+          {
+            $match: {
+              _id: new ObjectId(orderId),
+            },
+          },
+          {
+            $lookup: {
+              from: "order_items",
+              localField: "_id",
+              foreignField: "order_id",
+              as: "order_items",
+            },
+          },
+        ])
+        .toArray();
 
       if (!order) {
         return res.status(404).send({ error: "Order not found" });
       }
 
-      res.send(order);
+      res.send(order[0]);
     } catch (error) {
       console.error("Error");
       res.status(500).send({ error: "Internal Server Error" });
@@ -259,7 +282,6 @@ export const connectDB = async () => {
 
   // Products API endpoints
   const productsCollection = await database.collection("products");
-
   app.get("/products", async (req, res) => {
     try {
       const products = await productsCollection.find().toArray();
@@ -343,6 +365,7 @@ export const connectDB = async () => {
     const updatedStock = {
       $set: { stock },
     };
+
     try {
       const result = await productsCollection.updateOne(filter, updatedStock);
       const product = await productsCollection.findOne(filter);
@@ -360,6 +383,143 @@ export const connectDB = async () => {
       res.send({ "Stock updated": product });
     } catch (error) {
       console.error("Error creating product:", error);
+      res.status(500).send({ error: "Internal Server Error" });
+    }
+  });
+
+  // Order items API endpoints
+  const orderItemsCollection = await database.collection("order_items");
+  app.get("/orders/:id/items", async (req, res) => {
+    const orderId = req.params.id;
+
+    console.log(orderId);
+
+    try {
+      const items = await ordersCollection
+        .aggregate([
+          {
+            $match: {
+              _id: new ObjectId(orderId),
+            },
+          },
+
+          {
+            $lookup: {
+              from: "order_items",
+              localField: "_id",
+              foreignField: "order_id",
+              as: "order_item",
+            },
+          },
+          {
+            $unwind: {
+              path: "$order_item",
+            },
+          },
+          {
+            $lookup: {
+              from: "products",
+              localField: "order_item.product_id",
+              foreignField: "_id",
+              as: "order_item.product",
+            },
+          },
+          {
+            $unwind: "$order_item.product",
+          },
+          {
+            $project: {
+              _id: 0,
+              order_item_id: "$order_item._id",
+              quantity: "$order_item.quantity",
+              product_id: "$order_item.product_id",
+              title: "$order_item.product.title",
+              description: "$order_item.product.description",
+              product_price: "$order_item.product.price",
+            },
+          },
+        ])
+        .toArray();
+
+      console.log(items);
+
+      res.send(items);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ error: "Internal Server Error" });
+    }
+  });
+
+  app.post("/order-items", async (req, res) => {
+    const { order_id, product_id, quantity } = req.body;
+
+    if (!order_id || !product_id || !quantity) {
+      return res.status(400).send({ error: "All fields are required" });
+    }
+
+    const newOrderItem = {
+      order_id,
+      product_id,
+      quantity,
+    };
+
+    try {
+      const result = await orderItemsCollection.insertOne(newOrderItem);
+      res.send({ message: "Order item created", id: result.insertedId });
+    } catch (error) {
+      console.error("Error creating order item:", error);
+      res.status(500).send({ error: "Internal Server Error" });
+    }
+  });
+
+  app.patch("/order-items/:id", async (req, res) => {
+    const orderItemId = req.params.id;
+    const filter = { _id: new ObjectId(orderItemId) };
+
+    const { quantity } = req.body;
+
+    const updatedQuantity = {
+      $set: { quantity },
+    };
+
+    try {
+      const result = await orderItemsCollection.updateOne(
+        filter,
+        updatedQuantity
+      );
+      const orderItem = await orderItemsCollection.findOne(filter);
+
+      if (typeof quantity !== "number" || quantity < 0) {
+        return res
+          .status(400)
+          .send({ error: "Quantity must be a non-negative number" });
+      }
+
+      if (result.matchedCount === 0) {
+        return res.status(404).send({ error: "Order iten not found" });
+      }
+
+      res.send({ "Quantity updated": orderItem });
+    } catch (error) {
+      console.error("Error creating product:", error);
+      res.status(500).send({ error: "Internal Server Error" });
+    }
+  });
+
+  app.delete("/order-items/:id", async (req, res) => {
+    const orderItemId = req.params.id;
+
+    try {
+      const result = await orderItemsCollection.deleteOne({
+        _id: new ObjectId(orderItemId),
+      });
+
+      if (result.deletedCount === 0) {
+        return res.status(400).send({ error: "Could not find order item" });
+      }
+      res.send({ "Order item deleted": result });
+    } catch (error) {
+      console.error("Error creating order item:", error);
       res.status(500).send({ error: "Internal Server Error" });
     }
   });
