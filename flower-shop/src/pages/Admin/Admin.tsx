bc88567cb8d53483b7195361d2ba9f5fdca0ecaf
Jonna Luthman <jonna.luthman@medieinstitutet.se>
2025-05-06 11:56:00 +0100

    Added style to Adminpage. Small fixes with styling.

--- flower-shop/src/pages/Admin/Admin.tsx
+++ flower-shop/src/pages/Admin/Admin.tsx
@@ -1,6 +1,17 @@
+import { Link } from "react-router";
+import "../../styles/adminGeneral.css"
+
 export const Admin = () => {
   return (
-    <div>Admin</div>
-  )
-}
-
+    <div>
+      <h2>Admin managemant</h2>
+      <p>This is where you can see orders and manage products on your website.</p>
+      <button className="admin-button">
+        <Link to={"/admin/orders"}>Orders</Link>
+      </button>
+      <button className="admin-button">
+        <Link to={"/admin/products"}>Products</Link>
+      </button>
+    </div>
+  );
+};
