a7372a68299311c46a55636d3d69ec6d3d286ea7
Jonna Luthman <jonna.luthman@medieinstitutet.se>
2025-05-06 09:54:19 +0100

    Added very simple content for homepage.

--- flower-shop/src/pages/Homepage.tsx
+++ flower-shop/src/pages/Homepage.tsx
@@ -1,5 +1,15 @@
-import React from "react";
+import { Link } from "react-router";
+import "../styles/homepage.css"
 
 export const Homepage = () => {
-  return <div>Homepage</div>;
+  return <div className="homepage">
+  <h1>Welcome to Flower</h1>
+  <p>Discover our beautiful products and make your home bloom!</p>
+
+  <div className="homepage-actions">
+    <Link to="/products">
+      <button>Browse Products</button>
+    </Link>
+  </div>
+</div>
 };
